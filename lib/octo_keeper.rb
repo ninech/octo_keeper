c6a77be0ef7d0701981b1e2ff01ac7940bfb75a7
Philippe Hässig <phil@nine.ch>
2017-05-30 12:46:17 +0200

    Show organizations

--- lib/octo_keeper.rb
+++ lib/octo_keeper.rb
@@ -1,5 +1,12 @@
 require "octo_keeper/version"
 require "octo_keeper/cli"
 
+require "pastel"
+
 module OctoKeeper
+  ORGANIZATION = 'ninech'.freeze
+
+  def self.pastel
+    @pastel ||= Pastel.new
+  end
 end
--- lib/octo_keeper/cli.rb
+++ lib/octo_keeper/cli.rb
@@ -1,6 +1,27 @@
 require 'thor'
+require 'octokit'
+require 'pry'
+require 'tty-table'
 
 module OctoKeeper
   class CLI < Thor
+    desc "repos", "Shows our repositories on Github"
+    def repos
+      table = TTY::Table.new header: %w(Name Description)
+
+      client.org_repos(OctoKeeper::ORGANIZATION, type: 'all').each do |repo|
+        table << [repo.name, repo.description.to_s[0..50]]
+      end
+
+      puts table.render(:ascii)
+      puts ""
+      puts OctoKeeper.pastel.green("Currently we have #{OctoKeeper.pastel.bold(table.size[0])} repositories.")
+    end
+
+    private
+
+    def client
+      @client ||= Octokit::Client.new
+    end
   end
 end
