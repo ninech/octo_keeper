77149460e526f8c37843ead9b3ae4c4946703bd5
Philippe Hässig <phil@nine.ch>
2017-05-30 13:14:41 +0200

    List teams for the org

--- lib/octo_keeper/cli.rb
+++ lib/octo_keeper/cli.rb
@@ -7,21 +7,35 @@ module OctoKeeper
   class CLI < Thor
     desc "repos", "Shows our repositories on Github"
     def repos
-      table = TTY::Table.new header: %w(Name Description)
+      table_output(%w(Name Description)) do |table|
+        client.org_repos(OctoKeeper::ORGANIZATION, type: 'all').each do |repo|
+          table << [repo.name, repo.description.to_s[0..50]]
+        end
+      end
+    end
 
-      client.org_repos(OctoKeeper::ORGANIZATION, type: 'all').each do |repo|
-        table << [repo.name, repo.description.to_s[0..50]]
+    desc "teams", "List the organization's teams."
+    def teams
+      table_output(%w(ID Name Description)) do |table|
+        client.org_teams(OctoKeeper::ORGANIZATION).each do |team|
+          table << [team.id, team.slug, team.description.to_s[0..50]]
+        end
       end
 
-      puts table.render(:ascii)
-      puts ""
-      puts OctoKeeper.pastel.green("Currently we have #{OctoKeeper.pastel.bold(table.size[0])} repositories.")
     end
 
     private
 
     def client
       @client ||= Octokit::Client.new
+    end
+
+    def table_output(header)
+      table = TTY::Table.new header: header
+      yield table
+      puts table.render(:ascii)
+      puts ""
+      puts OctoKeeper.pastel.green("The list has #{OctoKeeper.pastel.bold(table.size[0])} items.")
     end
   end
 end
